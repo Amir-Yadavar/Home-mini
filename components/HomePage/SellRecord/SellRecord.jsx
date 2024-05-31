c28d13ee9cd7e7bcacbb23c0410f609daa4fe3d2
amir <amiresya@gmail.com>
2024-05-31 12:40:12 +0330

    fix bug ..

--- components/HomePage/SellRecord/SellRecord.jsx
+++ components/HomePage/SellRecord/SellRecord.jsx
@@ -1,26 +1,28 @@
-import React from "react";
 import styles from "@/styles/SellRecord.module.css";
 
 function SellRecord() {
   
-  const scrollHandler = ()=>{
-    console.log("ok");
-  }
-  
-    return (
-    <section className={styles.secSell} onScroll={scrollHandler}>
+
+  return (
+    <section className={styles.secSell} id="secRecord">
       <div className="d-flex align-items-center justify-content-around h-100">
         <div className={styles.boxRecord}>
-          <span>+ 500</span>
+          <span data-goal="125" className="spanRecord">
+           +125
+          </span>
+          <span>سرمایه گذاری موفق</span>
+        </div>
+        <div className={styles.boxRecord}>
+          <span data-goal="210" className="spanRecord">
+            +210
+          </span>
           <span>معامله موفق</span>
         </div>
         <div className={styles.boxRecord}>
-          <span>+ 500</span>
-          <span>معامله موفق</span>
-        </div>
-        <div className={styles.boxRecord}>
-          <span>+ 500</span>
-          <span>معامله موفق</span>
+          <span data-goal="89" className="spanRecord">
+            +89
+          </span>
+          <span>وام موفق</span>
         </div>
       </div>
     </section>
